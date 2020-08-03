730dd27ab546d6dde31be734fe6571dd8feb7d14
omayma <missaomayma@gmail.com>
2020-08-03 21:37:46 +0100

    Updates

---
+++ precache-manifest.d5a7736309b1715ffa0d6187b210fac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277c4b87045b53f0aa11685c38bf39e6",
+    "url": "/React-deploy/index.html"
+  },
+  {
+    "revision": "0a383b6a127d736afc16",
+    "url": "/React-deploy/static/css/main.6b5c6d34.chunk.css"
+  },
+  {
+    "revision": "340aac5933c756125f93",
+    "url": "/React-deploy/static/js/2.a0c31166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-deploy/static/js/2.a0c31166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a383b6a127d736afc16",
+    "url": "/React-deploy/static/js/main.23032e71.chunk.js"
+  },
+  {
+    "revision": "08a06da5808e43353941",
+    "url": "/React-deploy/static/js/runtime-main.4753e117.js"
+  },
+  {
+    "revision": "3c7eee45001b3590078d9501853af6dc",
+    "url": "/React-deploy/static/media/friends.3c7eee45.jpg"
+  }
+]);
